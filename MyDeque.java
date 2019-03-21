6fd9cf2e16acfdc94b38c1a72ad776a9d4033253
szou00 <szou00@stuy.edu>
2019-03-21 11:21:27 -0400

    wrote the constructors and a main

--- MyDeque.java
+++ MyDeque.java
@@ -2,17 +2,45 @@ public class MyDeque<E>{
   private E[] data;
   private int size, start, end;
 
+  @SuppressWarnings("unchecked")
   public MyDeque(){
-    E[] data = new E[10];
+    data = (E[])new Object[10];
+    size = 0;
+    start = 0;
+    end = 0;
+  }
+
+  @SuppressWarnings("unchecked")
+  public MyDeque(int initialCapacity){
+    data = (E[])new Object[initialCapacity];
+    size = initialCapacity;
+    start = 0;
+    end = initialCapacity-1;
+  }
+
+  public int size(){
+    return size;
+  }
+
+  public String toString(){
+    String ans = "";
+    for (int i = start; i < end+1;i++) {
+      ans += data[i] + " ";
+    }
+    return ans;
+  }
+
+  public void addFirst(E element){
 
   }
-  public MyDeque(int initialCapacity){  }
-  public int size(){ }
-  public String toString(){ }
-  public void addFirst(E element){ }
   public void addLast(E element){ }
-  public E removeFirst(){ }
-  public E removeLast(){ }
-  public E getFirst(){ }
-  public E getLast(){ }
+  // public E removeFirst(){ }
+  // public E removeLast(){ }
+  // public E getFirst(){ }
+  // public E getLast(){ }
+
+  public static void main(String[] args) {
+    MyDeque<String> test = new MyDeque<String>(8);
+    System.out.println(test);
+  }
 }
