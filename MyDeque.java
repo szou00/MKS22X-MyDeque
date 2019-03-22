d7a7175f7e5d1314d69f66cac7a6fceca971d871
szou00 <szou00@stuy.edu>
2019-03-22 11:00:38 -0400

    fixed addFirst and toString

--- MyDeque.java
+++ MyDeque.java
@@ -15,7 +15,7 @@ public class MyDeque<E>{
     data = (E[])new Object[initialCapacity];
     size = initialCapacity;
     start = 0;
-    end = initialCapacity-1;
+    end = 0;
   }
 
   public int size(){
@@ -24,24 +24,53 @@ public class MyDeque<E>{
 
   public String toString(){
     String ans = "";
-    for (int i = start; i < end+1;i++) {
-      ans += data[i] + " ";
+    for (int i = start; i < size;i++) {
+      if (data[i] != null) {
+        ans += data[i] + " ";
+      }
+    }
+    if (start > 0) {
+      for (int i = 0; i < start; i++) {
+        if (data[i] != null) {
+          ans += data[i] + " ";
+        }
+      }
     }
     return ans;
   }
 
   public void addFirst(E element){
-    
+    if (start == 0) {
+      data[start] = element;
+      start+=1;
+      end+=1;
+    }
+    else {
+      data[size-1] = element;
+      start = size-1;
+    }
   }
-  public void addLast(E element){ }
+
+  public void addLast(E element){
+    if (size == end) {
+      resize();
+    }
+  }
   // public E removeFirst(){ }
   // public E removeLast(){ }
   // public E getFirst(){ }
   // public E getLast(){ }
 
+  public void resize() {}
+
   public static void main(String[] args) {
     MyDeque<String> test = new MyDeque<String>(8);
     System.out.println(test);
-    System.out.println(test.size());
+    // System.out.println(test.size());
+    test.addFirst("h");
+    System.out.println(test);
+    test.addFirst("c");
+    System.out.println(test);
+
   }
 }
