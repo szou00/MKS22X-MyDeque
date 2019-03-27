719f01fb873e20de421d33dd965ed1a967110980
Sharon Zou <szou00@stuy.edu>
2019-03-27 11:18:03 -0400

    working to fix size

--- MyDeque.java
+++ MyDeque.java
@@ -16,7 +16,7 @@ public class MyDeque<E>{
   @SuppressWarnings("unchecked")
   public MyDeque(int initialCapacity){
     data = (E[])new Object[initialCapacity];
-    size = initialCapacity;
+    size = 0;
     start = -1;
     end = -1;
   }
@@ -26,49 +26,52 @@ public class MyDeque<E>{
   }
 
   public String toString(){
-    String ans = "{";
-    if (start == -1) {
-      return ans + "}";
+    String ans = "{"; //ans will store the contents of the string
+    if (start == -1) { //if there's nothing
+      return ans + "}"; //only {} is printed
     }
-    for (int i = start; i < size;i++) {
-      if (data[i] != null) {
-        ans += data[i] + " ";
+    for (int i = start; i < data.length;i++) { //otherwise, values from start to the end
+      if (data[i] != null) { //besides null
+        ans += data[i] + " "; //are added
       }
     }
-    if (start > 0) {
-      for (int i = 0; i < start; i++) {
-        if (data[i] != null) {
-          ans += data[i] + " ";
+    if (start > 0) { //if start wasn't zero
+      for (int i = 0; i < start; i++) { //need to circle back around and add all the values
+        if (data[i] != null) { //besides null
+          ans += data[i] + " "; //but up until start, to the end
         }
       }
     }
-    return ans + "}";
+    return ans + "}"; //other bracket is added
   }
 
   public void addFirst(E element){
-    if (element == null) {
+    if (element == null) { //if the element is null, throws an excpetion
       throw new NullPointerException();
     }
     // System.out.println("start: " + start + " end: " + end + " size: " + size);
-    if (start == -1) {
-      data[0] = element;
-      start = 0;
+    if (start == -1) { //if the list is empty
+      data[0] = element; //add to the list
+      start = 0; //both start and end get 0
       end = 0;
+      size++; //size increases
     }
     else {
-      if (start != 0 && data[start-1] != null) {
+      if (start != 0 && data[start-1] != null) { //if start isn't the beg of the list and there's no more
         // System.out.println("resizing");
-        resize();
+        resize(); //space to add to the front, resize
       }
-      if (start == 0) {
+      if (start == 0) { //if start is the beg already
         // System.out.println("start is 0");
-        data[size-1] = element;
-        start = size-1;
+        data[data.length-1] = element; //add element to the end
+        start = data.length-1; //move start
+        size++;
         return;
       }
       // System.out.println(Arrays.toString(data));
-      data[start-1] = element;
+      data[start-1] = element; //otherwise keeps adding to the one before start
       start--;
+      size++;
     }
   }
 
@@ -175,18 +178,22 @@ public class MyDeque<E>{
     System.out.println(test);
     // System.out.println(test.size());
     String str = "abcdefghijk";
-    // for (int i =  0;i < str.length();i++) {
-    //   test.addFirst(str.substring(i,i+1));
-    //   System.out.println(test);
-    // }
+    for (int i =  0;i < str.length();i++) {
+      test.addFirst(str.substring(i,i+1));
+      System.out.println(test);
+    }
     // for (int i =  0;i < str.length();i++) {
     //   test.removeFirst();
     //   System.out.println(test);
     // }
-    for (int i =  0;i < str.length();i++) {
-      test.addLast(str.substring(i,i+1));
-      System.out.println(test);
-    }
+    // for (int i =  0;i < str.length();i++) {
+    //   test.addLast(str.substring(i,i+1));
+    //   System.out.println(test);
+    // }
+    // for (int i =  0;i < str.length();i++) {
+    //   test.removeLast();
+    //   System.out.println(test);
+    // }
     // test.addFirst("c");
     // // test.removeFirst();
     // System.out.println(test);
