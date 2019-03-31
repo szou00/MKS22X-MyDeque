9a3e431d5d6b5a432068e5a471d2c1f50e40f718
Sharon Zou <szou00@stuy.edu>
2019-03-31 11:44:01 -0400

    all test cases from k's website seem to work

--- Calculator.java
+++ Calculator.java
@@ -61,5 +61,8 @@ public class Calculator{
 
     public static void main(String[] args) {
       System.out.println(eval("10 2.0 +"));
+      System.out.println(eval("11 3 - 4 + 2.5 *"));
+      System.out.println(eval("8 2 + 99 9 - * 2 + 9 -"));
+      System.out.println(eval("1 2 3 4 5 + * - -"));
     }
 }
