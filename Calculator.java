aa842bafc56fa76323aa96b0fa801bcc7eeae705
Sharon Zou <szou00@stuy.edu>
2019-03-31 11:39:56 -0400

    added in operators, currently works

--- Calculator.java
+++ Calculator.java
@@ -16,28 +16,50 @@ public class Calculator{
 
     public static double eval(String s){
       String[] ary = s.split(" "); //create an array of values and operators
-      System.out.println(Arrays.toString(ary)); //print out for testing
+      // System.out.println(Arrays.toString(ary)); //print out for testing
 
-      MyDeque operations = new MyDeque(ary.length); //create a MyDeque to use
+      MyDeque<Double> operations = new MyDeque<Double>(ary.length); //create a MyDeque to use
       int i = 0; //use this to go through the ary
       String operators = "+-*/%";
 
       while (i!=ary.length) { //while we haven't reached the end of the array
         if (!(operators.contains(ary[i]))) { //if the current value is not an operation
-          operations.addLast(Integer.parseInt(ary[i])); //add the value to the end of the deque
+          operations.addLast(Double.parseDouble(ary[i])); //add the value to the end of the deque
         }
         else { //otherwise remove the last two values and perform the operation on them, and then
           //add that back to the list
-          int result = operations.removeLast() + operations.removeLast();
-          //HARD CODE?? IS THERE ANY OTHER WAY
-
+          Double result = 0.0;
+          if (ary[i].equals("+")) { //if it's addition
+            // System.out.println("addition");
+            result = operations.removeLast() + operations.removeLast();
+          }
+          if (ary[i].equals("-")) {
+            Double subtracting = operations.removeLast(); //because 5 4 - is equal to 5-4 but 4 is the last
+            //value of the list right now!!
+            result = operations.removeLast() - subtracting;
+          }
+          if (ary[i].equals("*")) {
+            result = operations.removeLast() * operations.removeLast();
+          }
+          if (ary[i].equals("/")) {
+            Double divisor = operations.removeLast(); //similar to subtraction
+            result = operations.removeLast() / divisor;
+          }
+          if (ary[i].equals("%")) {
+            Double divisor = operations.removeLast();
+            result = operations.removeLast() % divisor;
+          }
+          //HARD CODE
+          // System.out.println(result);
           operations.addLast(result);
         }
+        i+=1;
+        // System.out.println(operations);
       }
-      return operations.getFirst(); //there should only be one element left in the array ?? so return that 
+      return operations.getFirst(); //there should only be one element left in the array ?? so return that
     }
 
     public static void main(String[] args) {
-      eval("10 2.0 +");
+      System.out.println(eval("10 2.0 +"));
     }
 }
